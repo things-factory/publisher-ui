abc54f1c7ae5781f9a59d8412081c0813f09fce2
Murky <moosung.gil@hatiolab.com>
2019-10-16 10:23:11 +0900

    fix. build

--- client/index.js
+++ client/index.js
@@ -1,3 +1,3 @@
-export { PublisherList as default } from './publisher-list'
 export { CreatePublisher } from './create-publisher'
 export { CrontabEditor } from './crontab-editor'
+export { PublisherList as default } from './publisher-list'
